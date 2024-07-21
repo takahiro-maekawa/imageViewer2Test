a2f6b36609333d3b22597d385ac48a17e49ddb86
takahiro-maekawa <koma4024@gmail.com>
2024-07-21 14:27:16 +0900

    imageソースを外から与えても動くことを確認

--- image_viewer_app/src/components/template/imageView/index.tsx
+++ image_viewer_app/src/components/template/imageView/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useEffect, useRef } from 'react';
 import { fileInfoType } from "@/types/fileInfoType"
+import Image from "next/image";
 
 import ImgViewerTestComp from "@/components/organism/imgViewerTestComp"
 import ImgViewerTestCompOverRay from '@/components/organism/imgViewerTestCompOverRay';
@@ -11,24 +12,18 @@ interface Props {
 }
 
 export default function ImageView({ data }: Props) {
-  /** 
-  
-  var $ = require('jQuery');
-
-  useEffect(() => {
-    if (holderRef.current) {
-      $(".sample").text("Hello World!");
-    }
-  }, []);
-  */
+  let src = "https://images.ygoprodeck.com/images/cards/62089826.jpg";
+  src = "https://images.ygoprodeck.com/images/cards/61901281.jpg";
+  src = "https://images.ygoprodeck.com/images/cards/51858306.jpg";
 
   return (
     <div className="p-2">
       <ImgViewerTestCompOverRay>
         <img
-          src={"https://images.ygoprodeck.com/images/cards/64202399.jpg"}
-          alt="Vercel Logo"
-
+          src={src}
+          alt="猫は最高に可愛い"
+          sizes="(max-width: 768px) 100vw, 50vw"
+          className="flex-1 w-full h-auto object-cover"
         />
       </ImgViewerTestCompOverRay>
     </div>
