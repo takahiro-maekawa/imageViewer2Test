4a5206c8443d675daaaa8527d99271f571a39a6f
takahiro-maekawa <koma4024@gmail.com>
2024-07-21 12:36:51 +0900

    ビューワー機能の完成

--- image_viewer_app/src/components/organism/imgViewerTestCompOverRay/index.tsx
+++ image_viewer_app/src/components/organism/imgViewerTestCompOverRay/index.tsx
@@ -1,66 +1,115 @@
 'use client'
 import { useEffect, useRef, useState } from 'react';
-import "leaflet/dist/leaflet.css";
-import { MapContainer, ImageOverlay, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // leafletのスタイルシートがないと、プラス-マイナスボタンが表示されない
+import { MapContainer, ImageOverlay, useMap, useMapEvent } from "react-leaflet";
 import { LatLng, LatLngBounds, CRS } from "leaflet";
 
+function MapControll({ setBounds }: { setBounds: React.Dispatch<React.SetStateAction<LatLngBounds>> }) {
+
+  // マップのサイズが変更された時にマップの移動範囲を更新するために準備
+  const map = useMap();
+
+  // ウィンドウそのもののサイズ変更を検知した場合の処理
+  const handleWindowResize = () => {
+    map.invalidateSize({ animate: false }); // これを外すとラグるので残します
+  };
+
+  // マウント時にイベントリスナーを追加
+  useEffect(() => {
+    // ウィンドウそのものにリサイズイベントを追加
+    window.addEventListener('resize', handleWindowResize);
+
+    // 描画境界を設定
+    setBounds(map.getBounds());
+
+    // マップの移動範囲を制限（これを外すと画像が描画範囲外に飛び出す）
+    map.setMaxBounds(map.getBounds());
+
+    // コンポーネントがアンマウントされたときにイベントリスナーを削除
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  // マップに対するリサイズイベント
+  // マップのサイズが変更された時にマップの移動範囲を更新するために準備
+  const onResize = () => {
+    setBounds(map.getBounds());
+    map.setMaxBounds(map.getBounds());
+  }
+  useMapEvent('resize', onResize);
+
+  return null;
+}
 
 export default function App({ children }: { children: React.ReactNode }) {
-  let ratio = 1185 / 813;
+  if (!children) {
+    return null;
+  }
 
-  const pixels = [1185, 813];
+  if (!children.props) {
+    return null;
+  }
 
-  const containerRef = useRef(null);
+  if (!children.props.src) {
+    return null;
+  }
 
-  const mapRef = useRef<HTMLDivElement>(null);
-  const [dimensions, setDimensions] = useState({ width: 370, height: 540 }); //現状は外から与える必要がある。
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState<{ width: number, height: number }>({ width: 33, height: 11 }); // 0以外であればなんでもいい
+  const [bounds, setBounds] = useState<LatLngBounds>(new LatLngBounds(new LatLng(0, 0), new LatLng(dimensions?.height / 2, dimensions?.width / 2)));
+  const zoom = 1;
+  const [src, setSrc] = useState<string>(children.props.src);
 
   useEffect(() => {
     if (containerRef.current) {
-      const observeTarget = containerRef.current;
-
-      const resizeObserver = new ResizeObserver(entries => {
-        entries.forEach(entry => {
-          setDimensions({
-            width: entry.contentRect.width,
-            height: entry.contentRect.height,
-          });
-        });
-        console.log("image Changed");
-      });
-
-      resizeObserver.observe(observeTarget);
-
-      return () => {
-        resizeObserver.unobserve(observeTarget);
-      };
+      const { width, height } = containerRef.current.getBoundingClientRect();
+      setDimensions({ width, height });
+      const latlng1 = new LatLng(0, 0);
+      const latlng2 = new LatLng(height / 2, width / 2);
+      setBounds(new LatLngBounds(latlng1, latlng2));
     }
-  }, []); // 依存配列を空にすることで、useEffect はコンポーネントがマウントされた後に一度だけ実行されます。
+  }, []);
 
-  const latlng1 = new LatLng(0, 0);
-  const latlng2 = new LatLng(dimensions.height / 2, dimensions.width / 2);
-  //const latlng2 = new LatLng(pixels[0] / 2, pixels[1] / 2);
+  // 子コンポーネントが変更されたときにsrcを更新
+  useEffect(() => {
+    if (!children || !children.props || !children.props?.src) {
+      return;
+    }
+    setSrc(children.props.src);
+  }, [children]);
 
-  const bounds = new LatLngBounds(latlng1, latlng2) // 1185 x 813 となっているので比率をうまく合わせれば問題ないはず
+  if (!dimensions) {
+    return null;
+  }
+
   return (
-    <div ref={containerRef} className="parent" style={{ position: "relative" }}>
-      {children}
-      <MapContainer
-        center={[0, 0]}
-        zoom={1}
-        crs={CRS.Simple}
-        scrollWheelZoom={true}
-        maxBounds={bounds} // マップの移動範囲を制限
-        maxBoundsViscosity={1.0} // マップの移動範囲を制限
-        minZoom={1}
-        maxZoom={10}
-        style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 10 }}
-        attributionControl={false} // デフォルトの著作権表示を非表示
-        className='overlay'
-      >
-        <ImageOverlay
-          url="https://images.ygoprodeck.com/images/cards/64202399.jpg" bounds={bounds} />
-      </MapContainer>
-    </div >
+    <>
+      <div ref={containerRef} className="parent" style={{ position: "relative" }}>
+        {children}
+
+        {/** リーフレットマップコンテナ */}
+        <MapContainer
+          center={[0, 0]}
+          zoom={zoom}
+          crs={CRS.Simple}
+          scrollWheelZoom={true}
+          maxBounds={bounds} // マップの移動範囲を制限
+          maxBoundsViscosity={1.0} // マップの移動範囲を制限
+          minZoom={1}
+          maxZoom={10}
+          style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 10 }}
+          attributionControl={false} // デフォルトの著作権表示を非表示
+          className='overlay'
+        >
+          {/** Map制御用のコンポーネント */}
+          <MapControll setBounds={setBounds} />
+
+          {/** 画像 */}
+          <ImageOverlay url={src} bounds={bounds} />
+        </MapContainer>
+      </div >
+    </>
+
   );
 }
