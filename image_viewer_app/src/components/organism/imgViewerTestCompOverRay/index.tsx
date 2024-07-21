70dfc2ce0ad5e0f9777fb09d74538a80cd32d0be
takahiro-maekawa <koma4024@gmail.com>
2024-07-21 19:13:44 +0900

    不具合修正
    - 初期化処理時にzoom値を1に戻す処理を追加
    - ZoomAnimationをfalseに
    - styleやクラスを伝搬させられるように修正
    - 文法的におかしい要素が取り込まれた時にエラーを吐くように修正
    - そのたコメントを追加

--- image_viewer_app/src/components/organism/imgViewerTestCompOverRay/index.tsx
+++ image_viewer_app/src/components/organism/imgViewerTestCompOverRay/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { isValidElement, useEffect, useRef, useState } from 'react';
+import { isValidElement, ReactElement, useEffect, useRef, useState } from 'react';
 import "leaflet/dist/leaflet.css"; // leafletのスタイルシートがないと、プラス-マイナスボタンが表示されない
 import { MapContainer, ImageOverlay, useMap, useMapEvent } from "react-leaflet";
 import { LatLng, LatLngBounds, CRS } from "leaflet";
@@ -34,6 +34,7 @@ function MapControll({ setBounds }: { setBounds: React.Dispatch<React.SetStateAc
   // マップに対するリサイズイベント
   // マップのサイズが変更された時にマップの移動範囲を更新するために準備
   const onResize = () => {
+    map.setZoom(1);
     setBounds(map.getBounds());
     map.setMaxBounds(map.getBounds());
   }
@@ -42,21 +43,25 @@ function MapControll({ setBounds }: { setBounds: React.Dispatch<React.SetStateAc
   return null;
 }
 
-export default function App({ children }: { children: React.ReactNode }) {
+const App: React.FC<{ children: ReactElement<{ src: string }> | null }> = ({ children }: { children: React.ReactNode }) => {
+  // 子コンポーネントが有効な形式であることを確認
   if (!isValidElement(children)) {
-    return null;
+    throw new Error("Child Component is not valid");
   }
 
+  // src属性がない場合はエラーを表示
   if (!children.props.src) {
-    return null;
+    throw new Error("Child Component must have src attribute");
   }
 
   const containerRef = useRef<HTMLDivElement>(null);
   const [dimensions, setDimensions] = useState<{ width: number, height: number }>({ width: 0, height: 0 });
-  const [bounds, setBounds] = useState<LatLngBounds>(new LatLngBounds(new LatLng(0, 0), new LatLng(dimensions?.height / 2, dimensions?.width / 2)));
-  const zoom = 1;
+  const [bounds, setBounds] = useState<LatLngBounds>(new LatLngBounds(new LatLng(0, 0), new LatLng(0, 0)));
   const [src, setSrc] = useState<string>(children.props.src);
+  const zoom = 1;
+  const zoomStep = 1;
 
+  // div要素が持つ幾何学情報を取得 
   useEffect(() => {
     if (containerRef.current) {
       const { width, height } = containerRef.current.getBoundingClientRect();
@@ -65,39 +70,53 @@ export default function App({ children }: { children: React.ReactNode }) {
       const latlng2 = new LatLng(height / 2, width / 2);
       setBounds(new LatLngBounds(latlng1, latlng2));
     }
-  }, []);
+  }, [containerRef]);
 
-  // 子コンポーネントが変更されたときにsrcを更新
-  // 記事を公開するタイミングでは消すかもしれない
+  // 画像がローディング中かどうかを判定
+  const [loading, setLoading] = useState<boolean>(true);
 
+  // 子コンポーネントが変更されたとき(画像のロードに成功した時も含む)にsrcを更新
   useEffect(() => {
+    setLoading(true);
+    const img = new Image();
+    img.src = children.props.src;
+    img.onload = () => setLoading(false);
     setSrc(children.props.src);
   }, [children]);
+
+  // ロード中であればLoadingを表示
+  // ロードできない場合にもLoading...と表示されるので注意
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // 子要素が持っているスタイルやクラスの情報
   const childStyle = children.props.style || {};
   const childClassName = children.props.className || '';
 
   return (
     <>
-      <div ref={containerRef} className="parent" style={{ position: "relative" }}>
+      <div ref={containerRef} style={{ position: "relative" }}>
         {children}
-
         {/** リーフレットマップコンテナ */}
         <MapContainer
           center={[0, 0]}
-          zoom={zoom}
+          zoomAnimation={false}
           crs={CRS.Simple}
           scrollWheelZoom={true}
           maxBounds={bounds} // マップの移動範囲を制限
           maxBoundsViscosity={1.0} // マップの移動範囲を制限
+          zoom={zoom}
           minZoom={1}
           maxZoom={10}
-          style={{ ...childStyle, position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 10 }}
+          zoomDelta={zoomStep}
+          wheelPxPerZoomLevel={Math.round(36 / zoomStep)} // ホイールでのズーム速度
           attributionControl={false} // デフォルトの著作権表示を非表示
+          style={{ ...childStyle, position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 10, padding: 0, margin: 0 }}
           className={childClassName}
         >
           {/** Map制御用のコンポーネント */}
           <MapControll setBounds={setBounds} />
-
           {/** 画像 */}
           <ImageOverlay url={src} bounds={bounds} />
         </MapContainer>
@@ -106,3 +125,5 @@ export default function App({ children }: { children: React.ReactNode }) {
 
   );
 }
+
+export default App;
