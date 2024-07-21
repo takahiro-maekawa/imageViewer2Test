6d2b298ef9491d6ded9a6939431d704fb5ac5a0b
takahiro-maekawa <koma4024@gmail.com>
2024-07-21 19:15:31 +0900

    イメージソースの修正

--- image_viewer_app/src/components/template/imageView/index.tsx
+++ image_viewer_app/src/components/template/imageView/index.tsx
@@ -2,10 +2,7 @@
 import React, { useEffect, useRef } from 'react';
 import { fileInfoType } from "@/types/fileInfoType"
 import Image from "next/image";
-
-import ImgViewerTestComp from "@/components/organism/imgViewerTestComp"
 import ImgViewerTestCompOverRay from '@/components/organism/imgViewerTestCompOverRay';
-import ImageWithFallback from '@/components/atom/imageWithFallBack';
 
 interface Props {
   data: fileInfoType[]
@@ -15,6 +12,8 @@ export default function ImageView({ data }: Props) {
   let src = "https://images.ygoprodeck.com/images/cards/62089826.jpg";
   src = "https://images.ygoprodeck.com/images/cards/61901281.jpg";
   src = "https://images.ygoprodeck.com/images/cards/51858306.jpg";
+  src = "http://dies-p.net/img/main_image00.jpg";
+  src = "https://images.ygoprodeck.com/images/cards/78872731.jpg";
 
   return (
     <div className="p-2">
@@ -23,7 +22,7 @@ export default function ImageView({ data }: Props) {
           src={src}
           alt="sampleImage"
           sizes="(max-width: 768px) 100vw, 50vw"
-          className="flex-1 w-full h-auto object-cover border-4 border-red-300"
+          className="flex-1 w-full h-auto object-cover border-4 border-blue-500 my-3"
           style={{ borderRadius: '70px' }}
         />
       </ImgViewerTestCompOverRay>
