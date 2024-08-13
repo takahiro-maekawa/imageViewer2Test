3d450bc8d80042549d84ce316565bc5fbfa42135
takahiro-maekawa <koma4024@gmail.com>
2024-08-13 22:14:51 +0900

    取得するデータを調整

--- image_viewer_app/src/actions/fileActions.ts
+++ image_viewer_app/src/actions/fileActions.ts
@@ -1,21 +1,12 @@
 import { fileInfoType } from "@/types/fileInfoType";
 
-/*
-        <ImageOnLabel labelText={"ラベル"} imageUrl={"https://images.ygoprodeck.com/images/cards/62089826.jpg"} className="w-1/6" />
-        <ImageOnLabel labelText={"ディープオブ"} imageUrl={"https://images.ygoprodeck.com/images/cards/64202399.jpg"} className="w-1/6" />
-        <ImageOnLabel labelText={"ブルーアイズ"} imageUrl={"https://images.ygoprodeck.com/images/cards/23995348.jpg"} className="w-1/6" />
-        <ImageOnLabel labelText={"ff"} imageUrl={"https://images.ygoprodeck.com/images/cards/23995348A.jpg"} className="w-1/6" />
-        <ImageOnLabel labelText={"QQQ"} imageUrl={"https://images.ygoprodeck.com/images/cards/23995348A.jpg"} className="w-1/6" />
-        <ImageOnLabel labelText={"ブルーアイズ"} imageUrl={"https://images.ygoprodeck.com/images/cards/23995348.jpg"} className="w-1/6" />
-        <ImageOnLabel labelText={"ブルーアイズ"} imageUrl={"https://images.ygoprodeck.com/images/cards/239953408.jpg"} className="w-1/6" />
-*/
 export const getData = async () => {
   const data = [
     { id: 1, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/62089826.jpg" },
     { id: 2, text: "label2", imageUrl: "https://images.ygoprodeck.com/images/cards/23995348.jpg" },
-    { id: 3, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/62089826.jpg" },
-    { id: 4, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/62089826.jpg" },
-    { id: 5, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/62089826.jpg" },
-    { id: 6, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/62089826.jpg" }];
+    { id: 3, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/73356503.jpg" },
+    { id: 4, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/572850.jpg" },
+    { id: 5, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/48905153.jpg" },
+    { id: 6, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/65734501.jpg" }];
   return data as fileInfoType[];
 }
