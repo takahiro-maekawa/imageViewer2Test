4d2804bde2cd5ca28d11f12c4a7d173b78c2989b
takahiro-maekawa <koma4024@gmail.com>
2024-08-25 13:59:13 +0900

    【Gyazo API】APIキーStateを準備し、Stateの状態によって表示される画像が変わることを確認 #9

--- image_viewer_app/src/app/test/page.tsx
+++ image_viewer_app/src/app/test/page.tsx
@@ -6,13 +6,15 @@ import Footer from '@/components/template/footer';
 import Header from '@/components/template/header';
 import ImageView from '@/components/template/imageView';
 import SelectArea from '@/components/template/selectArea';
+import ApiForm from '@/components/template/apiForm';
 import { fileInfoType, NONE } from '@/types/fileInfoType';
 import React, { useEffect, useState } from 'react';
 
 export default function RootLayout() {
   const [data, setData] = useState<fileInfoType[]>([]);
   const [targetData, setTargetData] = useState<fileInfoType>(NONE);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [apiKeyEncripted, setApiKeyEncripted] = useState<String>("");
 
   // データ更新を任意のタイミングで可能にするために、データ更新用の関数を準備
   const fetchAndSetData = async () => {
@@ -32,12 +34,21 @@ export default function RootLayout() {
       <Header />
       <div className="grid grid-cols-1 md:grid-cols-12">
         <div className="col-span-1"></div>
-        <div className="col-span-5">
-          {isLoading ? <><span>データ取り込み中</span></> : <SelectArea data={data} setTargetData={setTargetData} />}
-        </div>
-        <div className="col-span-5">
-          <ImageView data={targetData} />
-        </div>
+        {apiKeyEncripted == "" ?
+          <div className="col-span-10">
+            <ApiForm />
+          </div> :
+          <>
+            <div className="col-span-5">
+              {
+                isLoading ? <><span>データ取り込み中</span></> :
+                  <SelectArea data={data} setTargetData={setTargetData} />}
+            </div>
+            <div className="col-span-5">
+              <ImageView data={targetData} />
+            </div>
+          </>
+        }
         <div className="col-span-1"></div>
       </div>
       <Footer />
