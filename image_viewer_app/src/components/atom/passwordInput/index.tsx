e08ecf66a974244ca1812944e5a7102ddcc77e87
takahiro-maekawa <koma4024@gmail.com>
2024-08-25 15:27:17 +0900

    【Gyazo API】パスワード入力のユーザビリティ向上 #16
    修正概要は以下の通り。
    - パスワードの表示非表示機能を追加
    - それに伴いライブラリをインストール

--- image_viewer_app/src/components/atom/passwordInput/index.tsx
+++ image_viewer_app/src/components/atom/passwordInput/index.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+const PasswordReveal = styled.span`
+  position: absolute;
+  left: auto;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+`;
+
 export default function PasswordInput() {
-  return (<div className="border-green-600">
-    <input id="apiKey" name="apiKey" type="password" className="bg-black p-2 px-2" placeholder="APIキー" />
-  </div >
+  // パスワード表示制御用のstate
+  const [isRevealPassword, setIsRevealPassword] = useState(true);
+
+  const togglePassword = () => {
+    setIsRevealPassword(prevState => !prevState);
+  };
+
+  return (<div style={{ position: 'relative' }} >
+    <input id="apiKey" name="apiKey" type={isRevealPassword ? 'text' : 'password'} className="bg-black p-2 px-2" placeholder="APIキー" />
+    <PasswordReveal
+      onClick={togglePassword}
+      role="presentation"
+    >
+      <FontAwesomeIcon icon={isRevealPassword ? faEye : faEyeSlash} />
+    </PasswordReveal>
+  </ div >
   )
 }
