4aa13d7ff93913325fffbdc5e919d3dda2f8ffc8
takahiro-maekawa <koma4024@gmail.com>
2024-08-25 14:51:20 +0900

    【Gyazo】 画像のURLをGyazoからに変更 #18

--- image_viewer_app/src/actions/fileActions.ts
+++ image_viewer_app/src/actions/fileActions.ts
@@ -2,12 +2,12 @@ import { fileInfoType } from "@/types/fileInfoType";
 
 export const getData = async () => {
   const data = [
-    { id: 1, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/62089826.jpg" },
-    { id: 2, text: "label2", imageUrl: "https://images.ygoprodeck.com/images/cards/23995348.jpg" },
-    { id: 3, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/73356503.jpg" },
-    { id: 4, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/572850.jpg" },
-    { id: 5, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/14536035.jpg" },
-    { id: 6, text: "label1", imageUrl: "https://images.ygoprodeck.com/images/cards/65734501.jpg" }];
+    { id: 1, text: "label1", imageUrl: "https://i.gyazo.com/1c6defc638f71abd065d8dd2f450b207.jpg" },
+    { id: 2, text: "label2", imageUrl: "https://i.gyazo.com/7c0573bd6609c7179e296b8123c30054.jpg" },
+    { id: 3, text: "label1", imageUrl: "https://i.gyazo.com/9a1bae244482902935c9987040c7f5cb.png" },
+    { id: 4, text: "label1", imageUrl: "https://i.gyazo.com/343b83a0b0658de1d08419cfb21f249e.png" },
+    { id: 5, text: "label1", imageUrl: "https://i.gyazo.com/175226ec9f3800be54e3f76c1c4d2dca.png" },
+    { id: 6, text: "label1", imageUrl: "https://i.gyazo.com/44bff72b01214b2418ceccd8ae86a6ca.png" }];
 
   await new Promise(resolve => setTimeout(resolve, 1000));
   return data as fileInfoType[];
