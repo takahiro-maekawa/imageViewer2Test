86f5e3309296f628b48be0362cb799f40daf4171
takahiro-maekawa <koma4024@gmail.com>
2024-08-25 15:38:42 +0900

    ウィンドウ読み込み時に目のアイコンだけ、なぜか先に呼ばれていて目障り #19
    とりあえず遅延ロードでお茶を濁しているけど、なんか意味わかんねぇ

--- image_viewer_app/src/components/atom/passwordInput/index.tsx
+++ image_viewer_app/src/components/atom/passwordInput/index.tsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import dynamic from "next/dynamic";
+
+const FontAwesomeIcon = dynamic(() => import('@fortawesome/react-fontawesome').then(mod => mod.FontAwesomeIcon), { ssr: true });
 
 const PasswordReveal = styled.span`
   position: absolute;
   left: auto;
   right: 10px;
   top: 10px;
+  height: auto;
   cursor: pointer;
 `;
 
 export default function PasswordInput() {
   // パスワード表示制御用のstate
-  const [isRevealPassword, setIsRevealPassword] = useState(true);
+  const [isRevealPassword, setIsRevealPassword] = useState(false);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const togglePassword = () => {
     setIsRevealPassword(prevState => !prevState);
@@ -21,12 +29,14 @@ export default function PasswordInput() {
 
   return (<div style={{ position: 'relative' }} >
     <input id="apiKey" name="apiKey" type={isRevealPassword ? 'text' : 'password'} className="bg-black p-2 px-2" placeholder="APIキー" />
-    <PasswordReveal
-      onClick={togglePassword}
-      role="presentation"
-    >
-      <FontAwesomeIcon icon={isRevealPassword ? faEye : faEyeSlash} />
-    </PasswordReveal>
-  </ div >
+    {isClient && (
+      <PasswordReveal
+        onClick={togglePassword}
+        role="presentation"
+      >
+        <FontAwesomeIcon icon={isRevealPassword ? faEye : faEyeSlash} />
+      </PasswordReveal>
+    )}
+  </div >
   )
 }
