c710bdcae595040d8af05327190cd013eded5907
takahiro-maekawa <koma4024@gmail.com>
2024-08-29 01:06:59 +0900

    Dockefileの最適化 #2
    - standaloneモードで起動
    - エクステンションのバージョンアップ
    - run.shを作成
    - キャッシュディレクトリのシンボリックリンクを準備

--- image_viewer_app/next.config.js
+++ image_viewer_app/next.config.js
@@ -15,6 +15,7 @@ const nextConfig = {
   serverRuntimeConfig: {
     myPort: process.env.PORT || 3000, // 環境変数PORTが設定されていない場合は3000を使用
   },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
