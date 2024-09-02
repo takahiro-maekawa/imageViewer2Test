d50679b9442950219ad36ade816cb5560466d71b
takahiro-maekawa <koma4024@gmail.com>
2024-09-03 02:33:21 +0900

    アプリケーション全体に認証保護をかける(ローカル上) #14
    とりあえずfeatureブランチにpush
    アプリケーションを立ち上げる際に必ずGoogle認証が必要になった

---
+++ image_viewer_app/src/lib/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      return !!auth;
+    },
+  },
+});
