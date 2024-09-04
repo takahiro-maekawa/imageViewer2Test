012d2baeb30e1bcded0d059d08a60851d55269c8
takahiro-maekawa <koma4024@gmail.com>
2024-09-04 10:57:20 +0900

    2つのアプリケーションの疎通確認 #5
    Welcomeからメインのアプリにリダイレクトできることを確認

--- welcome/src/app/page.tsx
+++ welcome/src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+'use server';
 
-export default function Home() {
+import { redirect } from 'next/navigation';
+export default async function Home() {
+  const action = async () => {
+    'use server';
+    redirect("http://localhost:3000");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <span></span>
+        <span>ようこそ</span>
+        <form action={action}>
+          <button>CLICK</button>
+        </form>
       </div>
     </main>
   );
