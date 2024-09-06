2fd300e165db99d26311ea33291b72ee5626c5c9
takahiro-maekawa <koma4024@gmail.com>
2024-09-06 09:08:59 +0900

    Lambdaでホスティングしたサービスで認証できるようにする #21
    環境変数を適切に入れてみた

--- image_viewer_app/next.config.js
+++ image_viewer_app/next.config.js
@@ -15,6 +15,13 @@ const nextConfig = {
   serverRuntimeConfig: {
     myPort: process.env.PORT || 3000, // 環境変数PORTが設定されていない場合は3000を使用
   },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+  },
   output: 'standalone',
 }
 
