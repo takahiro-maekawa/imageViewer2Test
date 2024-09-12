50c6f92479bf44e405c10e8434a6d5e3afe83399
takahiro-maekawa <koma4024@gmail.com>
2024-09-12 23:41:38 +0900

    アカウントに関する認証テーブルの設計とDDLの作成 #36

--- database/diesel_schema/migrations/2024-09-06-133049_create_projects/down.sql
+++ database/diesel_schema/migrations/2024-09-06-133049_create_projects/down.sql
@@ -1,2 +1,6 @@
+DROP TABLE IF EXISTS permission_allocation ;
+
 -- This file should undo anything in `up.sql`
-DROP TABLE app_user;
+DROP TABLE IF EXISTS app_user;
+
+DROP TABLE IF EXISTS app_team ;
--- database/diesel_schema/migrations/2024-09-06-133049_create_projects/up.sql
+++ database/diesel_schema/migrations/2024-09-06-133049_create_projects/up.sql
@@ -1,10 +1,40 @@
--- Your SQL goes here
 CREATE TABLE app_user (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    enable_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
-    disable_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
-    version INTEGER NOT NULL DEFAULT 1
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    dataowner VARCHAR(255) DEFAULT 'system',
+    regist_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
+    enable_end_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
+    version BIGINT NOT NULL DEFAULT 1
+);
+
+CREATE TABLE app_team (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    api_key VARCHAR(255) NOT NULL,
+    dataowner VARCHAR(255) DEFAULT 'system',
+    regist_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
+    enable_end_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
+    version BIGINT NOT NULL DEFAULT 1
+);
+
+CREATE TABLE permission_allocation (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT,
+    team_id BIGINT,
+    ordinary_read BOOLEAN DEFAULT FALSE,
+    hyper_read BOOLEAN DEFAULT FALSE,
+    auth_read BOOLEAN DEFAULT FALSE,
+    ordinary_write BOOLEAN DEFAULT FALSE,
+    hyper_write BOOLEAN DEFAULT FALSE,
+    auth_write BOOLEAN DEFAULT FALSE,
+    dataowner VARCHAR(255) DEFAULT 'system',
+    regist_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
+    enable_end_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
+    version BIGINT NOT NULL DEFAULT 1,
+    FOREIGN KEY (user_id) REFERENCES app_user(id),
+    FOREIGN KEY (team_id) REFERENCES app_team(id)
 );
