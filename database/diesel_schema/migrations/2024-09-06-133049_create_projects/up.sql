c04e98630a8e787156cd8a7f5cf01636a6d462e3
takahiro-maekawa <koma4024@gmail.com>
2024-09-12 23:50:15 +0900

    アカウントに関する認証テーブルの設計とDDLの作成 #36
    システムカラムにNOT NULLはいらなかったかもしれない

--- database/diesel_schema/migrations/2024-09-06-133049_create_projects/up.sql
+++ database/diesel_schema/migrations/2024-09-06-133049_create_projects/up.sql
@@ -3,10 +3,10 @@ CREATE TABLE app_user (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     dataowner VARCHAR(255) DEFAULT 'system',
-    regist_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    regist_date TIMESTAMP DEFAULT current_timestamp,
     enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
     enable_end_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
-    version BIGINT NOT NULL DEFAULT 1
+    version BIGINT DEFAULT 1
 );
 
 CREATE TABLE app_team (
@@ -14,10 +14,10 @@ CREATE TABLE app_team (
     name VARCHAR(255) NOT NULL,
     api_key VARCHAR(255) NOT NULL,
     dataowner VARCHAR(255) DEFAULT 'system',
-    regist_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    regist_date TIMESTAMP DEFAULT current_timestamp,
     enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
     enable_end_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
-    version BIGINT NOT NULL DEFAULT 1
+    version BIGINT DEFAULT 1
 );
 
 CREATE TABLE permission_allocation (
@@ -31,10 +31,10 @@ CREATE TABLE permission_allocation (
     hyper_write BOOLEAN DEFAULT FALSE,
     auth_write BOOLEAN DEFAULT FALSE,
     dataowner VARCHAR(255) DEFAULT 'system',
-    regist_date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    regist_date TIMESTAMP DEFAULT current_timestamp,
     enable_start_date TIMESTAMP DEFAULT '1970-01-01 00:00:00',
     enable_end_date TIMESTAMP DEFAULT '9999-12-31 23:59:59',
-    version BIGINT NOT NULL DEFAULT 1,
+    version BIGINT DEFAULT 1,
     FOREIGN KEY (user_id) REFERENCES app_user(id),
     FOREIGN KEY (team_id) REFERENCES app_team(id)
 );
