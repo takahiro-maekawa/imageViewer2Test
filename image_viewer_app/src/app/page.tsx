a3a8b819676e265289381923bf1ccdbc7254784c
takahiro-maekawa <koma4024@gmail.com>
2024-09-19 08:55:14 +0900

    軽微な修正

--- image_viewer_app/src/app/page.tsx
+++ image_viewer_app/src/app/page.tsx
@@ -1,5 +1,15 @@
-export default function Home() {
-  return (<>
-    トップページだ
-  </>)
+import React, { ReactNode } from 'react';
+
+interface PageProps {
+  children: ReactNode;
 }
+
+const Page: React.FC<PageProps> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default Page;
