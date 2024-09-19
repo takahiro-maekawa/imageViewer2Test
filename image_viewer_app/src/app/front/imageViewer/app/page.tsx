9a2ad29ebd81fb1ec5c145a53af838b0b763cb60
takahiro-maekawa <koma4024@gmail.com>
2024-09-19 09:44:08 +0900

    Actionsがこけるので解決を目指す #50
    imageViewer/app配下がクライアントコンポーネントに変わっていた為修正しました。

--- image_viewer_app/src/app/front/imageViewer/app/page.tsx
+++ image_viewer_app/src/app/front/imageViewer/app/page.tsx
@@ -1,25 +1,16 @@
-'use client';
 import '@/app/globals.css';
 
 import { AppFront } from '@/components/template/appFront';
 import { redirectIfNotHaveAccount } from '@/lib/account/rooting';
-import React, { useEffect } from 'react';
 
 /**
  * appページ用のエンドポイント
  * @returns 
  */
-const DefaultLayout: React.FC = () => {
-  useEffect(() => {
-    const checkAccount = async () => {
-      await redirectIfNotHaveAccount();
-    };
-    checkAccount();
-  }, []);
+export default async function DefaultLayout() {
+  await redirectIfNotHaveAccount();
 
   return (
     <AppFront />
   );
-};
-
-export default DefaultLayout;
+}
