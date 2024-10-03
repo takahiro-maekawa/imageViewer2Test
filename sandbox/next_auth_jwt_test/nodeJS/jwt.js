fab149038037097a25192e5bbfde2a1d729d9252
takahiro-maekawa <koma4024@gmail.com>
2024-10-04 01:37:36 +0900

    【認証化PJ】Pythonでデコードするためのリサーチ #78
    - 参考となるサイトを見つけた
    - ノートブックに軽い実行コードを記述

--- sandbox/next_auth_jwt_test/nodeJS/jwt.js
+++ sandbox/next_auth_jwt_test/nodeJS/jwt.js
@@ -1,22 +1,36 @@
 import { decode, encode } from 'next-auth/jwt';
 
-const SESSION_NAME = 'authjs.session-token';
+const SALT = 'saltdesu';
 
-const object = { "sample": "data" };
+const object = { "sample": "datumaa" };
 
 const SECRET = 'secret';
 const encoded = await encode({
-  salt: SESSION_NAME,
+  salt: SALT,
   secret: SECRET,
   token: object,
 });
 
 // encoded: sample
-// eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoiN3g5RFVNVmFFVTZnaWFiMGwwT1dEdDRlNFNzUkhfY2J3TW9mMkV6RXl5Nm9uSzVvOHhTUFBCNzk5UlZvcW0ydjZ6Njc0a1FDdjlqb20yVDhTLWExZFEifQ..ZnnK_5jS-4wIadnKNzjOTw.7-R8orQxsU9p1hgQBdsnIbTN34jtDy-X4FSCr4koK90CpHmpLe4c9h2LZ_BnpmRIeS5I_3atvlv0_PuoO2Usm_apQEMTVgZZTBq4DB8m0NwP5YB964TBt7w0sUuzVzePMaQZ8WsYhzn1iVv0p-j6_A.Pgc_fxWGaaw5053g73PVBFbIZULBSDSxFM4AQcMhITo
-
+// eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwia2lkIjoiY1B4SGx3MXZGOXA1UXktRzlqSlVsVjBvZ1FZNTNPbzV1UTZwNHlNX0lFcXhnYlNacEJobjVBVnU5bmZMWmJxU0RaN0JMMU5KOFU3SnFfX0pVSHM2eGcifQ..UwJRcX9c9YLlH0_yJww7YQ.IEwxWszN5EkzWnfavqQTpOkviKu5V6PY7U4yzB-ZtP3AlF5TZr8yfO6zpN18Vr7h3ck48ZMVOdqArHL_GZtykzjxRQydSY5J6SDz_eMrWbS_xkoEiKjTUF98IlwgzUmdSRrSt9uA7igrF22Z32Ij7g.natRuwogAWvPhSjCtPicZhRuID72x9khE83RLTK98J8
+console.log("******************")
 console.log('encoded', encoded);
+
+/**
+ * 
+ * encryptionSecret: Uint8Array(64) [
+  177, 239, 108, 221, 145,  92,  68, 192, 167,  18,  50,
+   87,  74, 106, 112,  84, 106,  94,  22, 209,  43, 179,
+  141, 244,  99, 142,  59, 150, 189, 211,  32,  37,  19,
+   30, 144, 105,  95, 240, 114, 169, 209,  72, 241,  81,
+   28, 122,  73, 129, 168,  91,  99, 166,  40,  49,  29,
+  161, 250, 124, 207, 196, 156,  71, 194, 187
+]
+ * 
+ */
+
 const decoded = await decode({
-  salt: SESSION_NAME,
+  salt: SALT,
   secret: SECRET,
   token: encoded,
 });
@@ -24,10 +38,10 @@ const decoded = await decode({
 /**
  * 
  * decoded {
-  sample: 'data',
-  iat: 1727963444,
-  exp: 1730555444,
-  jti: 'ebdcc740-63a2-4087-b2e3-79a37411fdde'
-  }
+  sample: 'datumaa',
+  iat: 1727971115,
+  exp: 1730563115,
+  jti: '9fd25fec-edd5-4f89-9985-729a70b557e2'
+}
  */
 console.log('decoded', decoded);
---
+++ sandbox/next_auth_jwt_test/python/jwe_test/README.md
@@ -0,0 +1,7 @@
+# jwe-test
+
+Describe your project here.
+
+https://medium.com/@noahyoungs/decoding-auth-js-jwts-in-python-reverse-engineering-02deea5ce393
+
+による再現
